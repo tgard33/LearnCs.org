a8aee94eef06f1accc8be24cfbd7049e4f5b5ccf
Tyler Gardner <tyler.gardner@maersk.com>
2022-08-04 14:28:48 -0600

    Completed all exercises

--- Program.cs
+++ Program.cs
@@ -149,6 +149,119 @@ public class Tutorial
         }
 
         //Methods
+        //Write a method that divides two numbers (provided as parameters). Tip: you will need to use the
+        //modifiers public and static.
+        int xx = 2;
+        int y = 2;
+        int a = Foo(xx, y);
+        Console.WriteLine(a);
 
+        //Basic Classes
+        //Make a class car with the fields numTires = 4, year = 2000, and runs = true, and create 
+        //three instances of it: car1, car2, and car3.
+        Car car1 = new Car();
+        Car car2 = new Car();
+        Car car3 = new Car();
+       
+        // Test code
+        Console.WriteLine(car1.numTires);
+        Console.WriteLine(car2.year);
+        Console.WriteLine(car3.runs);
+
+        //Class Variables
+        //Make a class Vehicle with the fields string Type, int NumTires, int Year, and bool Runs, and create:
+
+        //A car: Type = car with NumTires = 4 from Year = 2000 which Runs = true An oldcar: Type = car with NumTires = 4 from 
+        //Year = 1980 which Runs = false A bike: Type = bike with NumTires = 2 from Year = 2017 which Runs = true
+        //Watch out for the capitalizations!
+        Vehicle car = new Vehicle("Car", 4, 2000, true);
+        Vehicle oldcar = new Vehicle("Car", 4, 1980, false);
+        Vehicle bike = new Vehicle("bike", 2, 2017, true);
+
+        // Test code
+        Console.WriteLine(car.Type);
+        Console.WriteLine(oldcar.Runs);
+        Console.WriteLine(bike.NumTires);
+
+        //Class Properties
+        //Make a class Vehicle with the properties string Type and int NumTires.
+        //When Type is set to String.Empty, print "The vehicle type cannot be empty" and keep the previous value.
+        //When NumTires is set to a value less than 1, print "The vehicle must have at least one tire" and keep the previous value.
+        // Test code
+        VehicleV2 myRide = new VehicleV2();
+        myRide.Type = "Motorcycle";
+        myRide.NumTires = 2;
+        Console.WriteLine(myRide.Type);
+        Console.WriteLine(myRide.NumTires);
+        myRide.Type = String.Empty;
+        Console.WriteLine(myRide.Type);
+        myRide.NumTires = 0;
+        Console.WriteLine(myRide.NumTires);
+    }
+
+    //Part of Class Properties Exercise
+    public class VehicleV2
+    {
+        private string _type;
+        public string Type
+        {
+            get { return _type; }
+            set
+            {
+                if (value == String.Empty)
+                {
+                    Console.WriteLine("The vehicle type cannot be empty");
+                }
+                else _type = value;
+            }
+        }
+
+        private int _numTires;
+        public int NumTires
+        {
+            get { return _numTires; }
+            set
+            {
+                if (value < 1)
+                {
+                    Console.WriteLine("The vehicle must have at least one tire");
+                }
+                else _numTires = value;
+            }
+        }
+    }
+
+    //Part of Class Variables Exercise
+    public class Vehicle
+    {
+        public string Type;
+        public int NumTires;
+        public int year;
+        public bool Runs;
+        public Vehicle(string type, int numTires, int year, bool runs)
+        {
+            Type = type;
+            NumTires = numTires;
+            this.year = year;
+            Runs = runs;
+        }   
+    }
+
+    //Part of Basic Classes Exercise
+    public class Car
+    {
+       public int numTires  = 4;
+       public string runs  = "true";
+       public int year  = 2000;
+       
+    }
+
+ 
+    //Part of Methods Exercise
+    public static int Foo(int xx, int y)
+    {
+        return xx / y;
     }
 }
+
+
